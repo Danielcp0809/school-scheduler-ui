a73b11ec5558eb93d5ce68bab8b48f664c61a9e8
Daniel Cordova <jdany0809@hotmail.com>
2023-10-31 19:24:15 -0500

    [Add] React persist added

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
 import { logger } from './middlewares/logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
@@ -16,14 +19,23 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 const altCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose || compose;
 
-const composedEnhancers = altCompose(applyMiddleware(logger)) 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export const store = createStore(rootReducer, composedEnhancers)
+const composedEnhancers = altCompose(applyMiddleware(logger)) 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, composedEnhancers)
+export const persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Navigation />
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
