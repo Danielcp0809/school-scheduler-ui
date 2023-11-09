14d649e746d9dd118119d69ed7fb1ad44d78b9e7
Daniel Cordova <jdany0809@hotmail.com>
2023-11-09 18:29:45 -0500

    [Update] Course view structure

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import { faChalkboardUser } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import View from "../containers/View/View";
 
 interface CoursesProps {}
 
 function Courses(props: CoursesProps) {
-    return (
-        <div>
-            Courses
-        </div>
-    );
+  return (
+    <View
+      title="Cursos"
+      icon={faChalkboardUser}
+      description="Crea y modifica las cursos de tu escuela."
+    >
+
+    </View>
+  );
 }
 
 export default Courses;
